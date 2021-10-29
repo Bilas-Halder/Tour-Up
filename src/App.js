9869db468c3bf9a7e8e5843e6082520003cdf9fa
Bilas-Halder <bilas8642@gmail.com>
2021-10-29 22:45:30 +0600

    Header Design Done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { initializeApp } from 'firebase/app';
+import firebaseConfig from './firebase/firebase_config';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from './Components/Home/Home';
+import Header from './Components/Header/Header';
+import AboutUs from './Components/AboutUs/AboutUs';
+import LogIn from './Components/LogIn/LogIn';
+import NotFound from './Components/NotFound/NotFound';
+
+
+initializeApp(firebaseConfig);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header > </Header>
+      <Switch>
+        <Route path='/' exact >
+          <Home></Home>
+        </Route>
+        <Route path="/aboutus" exact >
+          <AboutUs></AboutUs>
+        </Route>
+        <Route path="/login" exact >
+          <LogIn></LogIn>
+        </Route>
+        <Route path="/*" exact >
+          <NotFound></NotFound>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import logo from '../../images/logo.png'
+import './Header.css';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { HashLink } from 'react-router-hash-link';
+
+const Header = () => {
+    return (
+        <Navbar fixed='top' bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand as={HashLink} to="/">
+                    <div className="d-flex align-items-center justify-content-center">
+                        <div className="logo-div">
+                            <img src={logo} alt="" />
+                        </div>
+                        <h4 className="brand-name"> Tour-Up</h4>
+                    </div>
+                </Navbar.Brand>
+
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+                    <Nav.Link className="text-center nav-btn" as={HashLink} to="/">Home</Nav.Link>
+
+                    <Nav.Link className="text-center nav-btn" as={HashLink} to="/">Packages</Nav.Link>
+
+                    <Nav.Link className="text-center nav-btn" as={HashLink} to="/aboutus">About Us</Nav.Link>
+
+                    <Nav.Link className="text-center nav-btn" as={HashLink} to="/login">Log In</Nav.Link>
+
+
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;
